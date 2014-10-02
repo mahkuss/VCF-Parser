439b8f23538348f4521f72ac17286858d9980e7f
mahkus <anonymouse@xqua.org>
2014-10-02 15:08:12 -0400

    Initial commit.

---
+++ VCFparser.py
@@ -0,0 +1,161 @@
+"""
+Python tool for parsing VCF files.
+"""
+
+class VCFentry:
+	"""
+	Class object to parse and represent a single entry in a VCF file.
+	"""
+	def __init__(self, entry):
+		fields = entry.strip().split('\t')
+		self.CHROM, self.POS, self.ID, self.REF, self.ALT, self.QUAL, self.FILTER, self.INFO =\
+		fields[:8]
+
+		self.POS = int(self.POS)
+		self.QUAL = float(self.QUAL)
+
+		if len(fields) > 8:
+			self.FORMAT = fields[8]
+			self.GENOTYPE = [ genotype for genotype in fields[9:] ]
+
+		if ( len(self.REF) > 1 ) or ( len(self.ALT) > 1 ):
+			self.TYPE = 'indel'
+		else:
+			self.TYPE = 'snp'
+
+	def __str__(self):
+		return '\t'.join([ self.CHROM, self.POS, self.ID, self.REF, self.ALT, self.QUAL,\
+			self.FILTER, self.INFO, self.FORMAT, '\t'.join( [ genotype for genotype in self.GENOTYPE ] ) ] ) + '\n'
+
+
+class VCF:
+	"""
+	Parse a VCF file.
+	"""
+	def __init__(self, filename):
+		self.filename = filename
+		self.header = []
+		self.header_line_count = 0
+		with open(filename, 'r') as vcf:
+			for line in vcf:
+				if line[0] == '#':
+					self.header.append(line)
+					self.header_line_count += 1
+				else:
+					break
+
+
+	def split(self, writeSNP=True, writeINDEL=True):
+		"""
+		Class method to split a .vcf file into separate files containing SNPs and INDELs, respectively
+		"""
+
+		if writeSNP:
+			snp_filename = self.filename.split('.vcf')[0] + 'SNP.vcf'
+			snp_out = open(snp_filename, 'w')
+			for line in self.header:
+				snp_out.write(line)
+
+		if writeINDEL:
+			indel_filename = self.filename.split('.vcf')[0] + 'INDEL.vcf'
+			indel_out = open(indel_filename, 'w')
+			for line in self.header:
+				indel_out.write(line)
+
+		with open(self.filename, 'r') as vcf:
+			for ind, line in enumerate(vcf):
+				if ind < self.header_line_count:
+					continue
+				elif ( VCFentry(line).TYPE == 'snp' ) and writeSNP:
+					snp_out.write(line)
+				elif writeINDEL:
+					indel_out.write(line)
+
+		if writeSNP:
+			snp_out.close()
+
+		if writeINDEL:
+			indel_out.close()
+
+
+	def filter(self, output, quality=0, variant_type=['snp', 'indel'], genotype=False, chrom=False, column=1, coords=[0,1e10]):
+		"""
+		Class method to generate a new vcf file containing only calls passing a specified filter.
+		If genotype as specified (choices: homozygous, heterozygous) you must also specify which column
+		to check for multi-sample vcf files. By default, the first column will be checked. Current behavior
+		will check the genotype of the sample in that column, but will write the data for all samples
+		if the specified column passes the genotype filter.
+		"""
+
+		number_checked = 0
+		number_passed = 0
+
+		with open(output, 'w') as outfile:
+			for line in self.header:
+				outfile.write(line)
+
+			with open(self.filename, 'r') as vcf:
+				for ind, line in enumerate(vcf):
+					if ind < self.header_line_count:
+						#print('%s: header line' %ind)
+						continue
+
+					else:
+						current_line = VCFentry(line)
+						number_checked += 1
+						# check if chromosome filter set and, if so, is current line from that chromosome?
+						if chrom and current_line.CHROM not in chrom:
+							#print('%s: failed chrom filter' %ind)
+							continue
+
+						if ( current_line.POS < coords[0] ) or ( current_line.POS > coords[1] ):
+							#print('%s: failed coordinates filter' %ind)
+							continue
+
+						# check if genotype filter set and, if so, is current line of the correct genotype?
+						if genotype:
+							#print('%s: checking genotype' %ind)
+							current_line_genotype = current_line.GENOTYPE[ column - 1 ].split(':')[0]
+							if current_line_genotype.split('/')[0] == current_line_genotype.split('/')[1]:
+								current_line_genotype = 'homozygous'
+							else:
+								current_line_genotype = 'heterozygous'
+							#print('genotype = ' + current_line_genotype)
+							if current_line_genotype != genotype:
+								#print('%s: failed genotype test' %ind)
+								continue
+
+						# check that current line passes the quality and variant_type filters
+						if ( current_line.QUAL >= quality ) and ( current_line.TYPE in variant_type ):
+							# if you get this far, current line passes all filters and should be written to output file
+							#print('%s: passed all filters, writing to outfile' %ind)
+							outfile.write(line)
+							number_passed += 1
+
+		print('Variants checked: %s' %number_checked)
+		print('Variants passed: %s' %number_passed)
+
+
+	def qualityDistribution(self):
+		"""
+		Class method to plot distribution of quality scores in vcf file.
+		"""
+
+		import matplotlib.pyplot as plt
+
+		quality_scores = []
+
+		with open(self.filename, 'r') as vcf:
+			for ind, line in enumerate(vcf):
+				if ind < self.header_line_count:
+					continue
+
+				else:
+					quality_scores.append( VCFentry(line).QUAL )
+
+		plt.hist(filter(lambda x: x < 2000, quality_scores), bins=100)
+		plt.title('Quality Score Distribution')
+		plt.xlabel('Quality Score')
+		plt.ylabel('Frequency')
+		plt.show()
+
